1f6fc81e6fbd295faf1b7f84f20dbfd6a5368dca
maguscine <159982706+maguscine@users.noreply.github.com>
2024-02-13 23:29:11 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Favicon - FIS -->
+  <link rel="shortcut icon" href="admin/encuestas/logo.png">
+
+  <title>Carné universitario 2021</title>
+
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="javascript:void(0)">Carné universitario 2021</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!--NAVBAR-->
+      <div class="collapse navbar-collapse" id="navb">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <a class="btn btn-primary" href="login.php" role="button">Ingresar</a>
+        </form>
+      </div>
+  </nav>
+
+    <!--CAROUSEL-->
+
+    <div class="container" style="margin-top: 30px; margin-bottom: 30px;">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="imagenes/Grafico1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="admin/encuestas/logo.png" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="imagenes/Grafico1.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+
+     <form class="form-inline my-2 my-lg-0">
+          <a class="btn btn-primary" href="login.php" role="button">Ingresar</a>
+        </form>
+  
+    <!-- Footer -->
+    <footer class="page-footer font-small" style="background-color: #343A40; color: #FFF">
+
+      <!-- Copyright -->
+      <div class="footer-copyright text-center py-3">© 2020 Todos los derechos reservados
+        <br>
+        <a href="http://www.uncp.edu.pe/" target="_blank"> Unidad de Gestión Académica | UNCP</a>
+      </div>
+      <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
